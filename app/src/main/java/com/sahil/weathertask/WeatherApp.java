5e87b9708faabd7d81b7d8bb024ca850e1fcda7d
Sahil_Munjal <sahil5141.munjal@gmail.com>
2019-08-05 21:15:46 +0530

    Added App Class, required dependencies & res

---
+++ app/src/main/java/com/sahil/weathertask/WeatherApp.java
@@ -0,0 +1,11 @@
+package com.sahil.weathertask;
+
+import android.app.Application;
+
+public class WeatherApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
