b9e2cdf9c1f25733f62dc397a86d01f1b0b5830d
Sahil_Munjal <sahil5141.munjal@gmail.com>
2019-08-06 08:30:14 +0530

    Added Home Activity UI and assets

---
+++ app/src/main/java/com/sahil/weathertask/presentation/home/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.sahil.weathertask.presentation.home;
+
+import android.os.Bundle;
+
+import com.sahil.weathertask.R;
+import com.sahil.weathertask.presentation.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+    @Override
+    public int getLayoutRes() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    public void initializeViews(Bundle savedInstanceState) {
+
+    }
+}
